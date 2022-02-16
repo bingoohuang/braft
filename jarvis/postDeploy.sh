a41269c28a5d8129c15cf45e917dff8382139e7e
bingoohuang <bingoo.huang@gmail.com>
2022-02-16 17:00:31 +0800

    update postDeploy.sh

--- jarvis/postDeploy.sh
+++ jarvis/postDeploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+export GOPROXY=https://goproxy.cn
 make bin=braft jarvis
+
 mkdir -p build
-cp braft ./build && cp jarvis/* ./build
+cp braft ./build && cp jarvis/* ./build && rm -f ./build/postDeploy.sh
 mkdir -p output
-cd build && tar -cvzf ../output/ROOT.tar.gz *
+cd build && tar -cvzf ../output/ROOT.tar.gz * && tar -ztvf ../output/ROOT.tar.gz
